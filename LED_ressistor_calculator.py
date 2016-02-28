957e00b4bf969bb8aa7f9edd4b72fddf1fa4e12d
GhostGR <GhostGR@users.noreply.github.com>
2016-02-28 11:46:11 +0200

    Uploading main script.
    Uploading the "LED_ressistor_calculator.py" file.

---
+++ LED_ressistor_calculator.py
@@ -0,0 +1,54 @@
+# LED resistor calculator
+# Copyright (c) 2016 Lykourgos Tanious
+# Read LICENCE.txt
+# Description :      A script that calculates the required
+#               resistance and wattage to properly run a LED.
+# Author      : Lykourgos Tanious
+# date        : 09/02/2016
+# Version     : V1.0
+
+
+def main():
+    while True:
+        print("################################################\n"
+              "#         LED Resistor Value Calculator        #\n"
+              "#==============================================#\n"
+              "#     Copyright (c) 2015 Lykourgos Tanious     #\n"
+              "#----------------------------------------------#\n"
+              "#     Version : V1.0                           #\n"
+              "#     Author  : Lykourgos Tanious              #\n"
+              "################################################\n")
+        while True:
+            try:
+                vin = float(input("power supply voltage(V)? :  "))
+                break
+            except:
+                print("Invalid input, it must be a number")
+        while True:
+            try:
+                vled = float(input("LED forward voltage(V)? :  "))
+                break
+            except:
+                print("Invalid input, it must be a number")
+        while True:
+            try:
+                lma = float(input("operating current of the LED(ma)? :  "))
+                break
+            except:
+                print("Invalid input, it must be a number")
+        print("\n"
+              "Input Voltage          :  ", vin, "V\n"
+              "LED forward voltage    :  ", vled, "V\n"
+              "LED operating current  :  ", lma, "ma\n"
+              "------------------------------------------------\n"
+              "Resistor :  (", vin, "-", vled, ")/(", lma, "/1000) =  ", (vin-vled)/(lma/1000), "ohms\n"
+              "Wattage  :   ", vin, "*", "(", lma, "/1000", ")", "     =  ", (vin*(lma/1000)), "W\n"
+              "\n")
+        while True:
+            inp = input("Do you want to exit(Y/N)?  ")
+            if inp == "Y" or inp == "y":
+                exit(0)
+            elif inp == "N" or inp == "n":
+                break
+
+main()
